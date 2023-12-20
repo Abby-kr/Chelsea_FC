b6cd32cb1f9924f2d14aed2c62e76158ce622f60
Abigail <abby.a.otieno@gmail.com>
2023-12-20 17:15:29 +0300

    Added models

--- server/app.py
+++ server/app.py
@@ -1,10 +1,57 @@
-from flask import Flask
-
+from flask import Flask,make_response,jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_cors import CORS
+from models import db,MalePlayer,FemalePlayer
 
 app = Flask(__name__)
+CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///chelsea.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+migrate = Migrate(app, db)
+db.init_app(app)
 
+@app.route('/First_Team', methods=['GET'])
+def get_players():
+    players = MalePlayer.query.all()
+    players_list = []
+    for player in players:
+        players_list.append({
+            'id': player.id,
+            'image':player.image,
+            'name':player.name,
+            'number': player.playerNumber,
+            'position':player.position,
+            'nationality': player.nationality
+        })
+
+    response = make_response(
+        jsonify(players_list),
+        200
+    )
+
+    return response
+
+@app.route('/Womens_Team', methods=['GET'])
+def get_women_players():
+    players = FemalePlayer.query.all()
+    players_list = []
+    for player in players:
+        players_list.append({
+            'id': player.id,
+            'image':player.image,
+            'name':player.name,
+            'playerNumber': player.playerNumber,
+            'position':player.position,
+            'nationality': player.nationality
+        })
+
+    response = make_response(
+        jsonify(players_list),
+        200
+    )
+
+    return response
 
 if __name__ == '__main__':
     app.run(port=5555,rdebug=True)
---
+++ server/models.py
@@ -0,0 +1,29 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class MalePlayer(db.Model):
+    __tablename__ = 'male_players'
+
+    id = db.Column(db.Integer, primary_key=True)
+    image = db.Column(db.String, nullable=False)
+    name = db.Column(db.String, unique=True, nullable=False)
+    playerNumber = db.Column(db.String, unique=True, nullable=False)
+    position = db.Column(db.String, nullable=False)
+    nationality = db.Column(db.String, nullable=False)
+
+    def __repr__(self):
+        return f'<Player: {self.name}>'
+
+class FemalePlayer(db.Model):
+    __tablename__ = 'female_players'
+
+    id = db.Column(db.Integer, primary_key=True)
+    image = db.Column(db.String, nullable=False)
+    name = db.Column(db.String, unique=True, nullable=False)
+    playerNumber = db.Column(db.String, unique=True, nullable=False)
+    position = db.Column(db.String, nullable=False)
+    nationality = db.Column(db.String, nullable=False)
+
+    def __repr__(self):
+        return f'<Player: {self.name}>'
